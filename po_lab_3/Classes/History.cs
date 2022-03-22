2e71cc93139161f04009f2f805ec11edac0a2ef7
Michał Jakóbczyk <64917746+MichalJakobczyk1@users.noreply.github.com>
2022-03-22 15:00:44 +0100

    initial commit

---
+++ po_lab_3/Classes/History.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classes
+{
+    public class HistoryArray<T>
+    {
+        private T[] array = new T[1000];
+        private int last = -1;
+        public void Add(T item)
+        {
+            if (last == 1000)
+            {
+                last = 0;
+            }
+            array[++last] = item;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i <= last; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ po_lab_3/Classes/Reward.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Reward
+    {
+        public void GiveTo<T>(T target)
+        {
+            Console.WriteLine($"{target}! You got a reward");
+        }
+    }
+}
